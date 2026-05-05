4a00758c19c8fe864e571ffb61d08218f3587270
janderson01 <jandersonsantos030@gmail.com>
2026-05-05 00:17:06 -0300

    feat: adiciona rabbitMQ ao projeto, configura conxão e declara fila testado

--- worker/worker.py
+++ worker/worker.py
@@ -1,6 +1,31 @@
-import time
+import os
+import time  
+import pika
 
-if __name__ == "__main__":
-    print("Worker iniciado...")
-    while True:
+RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "rabbitmq")
+
+while True:
+    try:
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host=RABBITMQ_HOST)
+        )
+        break
+    except pika.exceptions.AMQPConnectionError:
+        print("RabbitMQ não disponível, tentando novamente em 5s...", flush=True)
         time.sleep(5)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='task_queue', durable=True)
+
+def callback(ch, method, properties, body):
+    print(f"[X] Received {body.decode()}", flush=True)
+
+channel.basic_consume(
+    queue='task_queue',
+    on_message_callback=callback,
+    auto_ack=True
+)
+
+print ("[*] Esperando por mensagens...", flush=True)
+channel.start_consuming()
