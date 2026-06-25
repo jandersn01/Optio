0580d6e535944717cb8b9541278622ee7fa5c880
Janderson Lima dos Santos <estagio.janderson@jfpb.jus.br>
2026-06-25 15:40:29 -0300

    refact: cria classe de servico para notificacao

---
+++ worker/infra/notifications.py
@@ -0,0 +1,10 @@
+from search.emails import send_results_email, send_no_results_email
+
+from search.emails import send_results_email, send_no_results_email
+
+class EmailNotificationService:
+    def send_no_results(self, user_email: str, search_id: int, keywords: str):
+        send_no_results_email(user_email=user_email, search_id=search_id, keywords=keywords)
+
+    def send_results(self, user_email: str, courses: list[dict], search_id: int):
+        send_results_email(user_email=user_email, courses=courses, search_id=search_id)
