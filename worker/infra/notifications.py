240799703314fd4986edfd318072c190581eed41
janderson01 <jandersonsantos030@gmail.com>
2026-06-28 04:13:12 -0300

    refact: cria servico de comunicação

--- worker/infra/notifications.py
+++ worker/infra/notifications.py
@@ -1,7 +1,5 @@
 from search.emails import send_results_email, send_no_results_email
 
-from search.emails import send_results_email, send_no_results_email
-
 class EmailNotificationService:
     def send_no_results(self, user_email: str, search_id: int, keywords: str):
         send_no_results_email(user_email=user_email, search_id=search_id, keywords=keywords)
