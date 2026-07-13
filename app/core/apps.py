6a601d9c99de5337ebf86ff0c782e11051e3a280
Janderson Lima dos Santos <estagio.janderson@jfpb.jus.br>
2026-07-13 15:10:41 -0300

    faz o django ouvir os sgnals do allauth

--- app/core/apps.py
+++ app/core/apps.py
@@ -5,3 +5,6 @@ class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
     verbose_name = 'Optio Core'
+    
+    def ready(self):
+        import core.signals  
